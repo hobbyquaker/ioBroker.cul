579b3c420a62c3d3add71e47a2b099463747aa9d
GermanBluefox <dogafox@gmail.com>
2015-03-03 19:00:35 +0100

    * (bluefox) try to bring the adapter to state of the art

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ ioBroker adapter to control FS20, Max!, HMS and other devices via [CUL](http://b
 [culfw](http://culfw.de). Depends on https://github.com/hobbyquaker/cul
 
 
+## Changelog
+### 0.0.3 (2015-03-03)
+* (bluefox) try to bring the adapter to state of the art
+
+
+
 ## License
 
 [Licensed under GPLv2](LICENSE) Copyright (c) 2014 hobbyquaker
--- cul.js
+++ cul.js
@@ -1,117 +1,120 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+
 var Cul = require('cul');
+"use strict";
+
+// you have to require the utils module and call adapter function
+var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 
 var cul;
 var objects = {};
 var metaRoles = {};
 
-var adapter = require(__dirname + '/../../lib/adapter.js')({
+var adapter = utils.adapter('cul');
 
-    name:           'cul',
+adapter.on('stateChange', function (id, state) {
+    //if (cul) cul.cmd();
+});
 
-    objectChange: function (id, obj) {
+adapter.on('unload', function (callback) {
+    if (cul) cul.close();
+    callback();
+});
 
-    },
+adapter.on('ready', function () {
+    main();
+});
 
-    stateChange: function (id, state) {
+function main() {
+    var options = {
+        serialport:     adapter.config.serialport || '/dev/ttyACM0',
+        baudrate:       adapter.config.baudrate   || 9600,
+        mode:           adapter.config.mode       || 'SlowRF'
+    };
 
-    },
+    cul = new Cul(options);
 
-    unload: function (callback) {
-        if (cul) cul.close();
-        callback();
-    },
-
-    ready: function () {
-
-        var options = {
-            serialport:     adapter.config.serialport   || '/dev/ttyACM0',
-            baudrate:       adapter.config.baudrate     || 9600,
-            mode:           adapter.config.mode         || 'SlowRF'
-        };
-
-        cul = new Cul(options);
-
-        adapter.objects.getObject('cul.meta.roles', function (err, res) {
-            metaRoles = res.native;
-            adapter.objects.getObjectView('cul', 'devices', function (err, res) {
-                for (var i = 0, l = res.total_rows; i < l; i++) {
-                    objects[res.rows[i].id] = res.rows[i].value;
-                }
-                receive();
-            });
+    adapter.objects.getObject('cul.meta.roles', function (err, res) {
+        metaRoles = res.native;
+        adapter.objects.getObjectView('cul', 'devices', function (err, res) {
+            for (var i = 0, l = res.total_rows; i < l; i++) {
+                objects[res.rows[i].id] = res.rows[i].value;
+            }
+            receive();
         });
+    });
 
-        cul.on('close', function () {
-            adapter.states.setState('system.adapter.' + adapter.namespace + '.connected', {val: false, ack: true});
-        });
+    cul.on('close', function () {
+        adapter.states.setState('system.adapter.' + adapter.namespace + '.connected', {val: false, ack: true});
+    });
 
-        cul.on('ready', function () {
-            adapter.states.setState('system.adapter.' + adapter.namespace + '.connected', {val: true, ack: true});
-        });
+    cul.on('ready', function () {
+        adapter.states.setState('system.adapter.' + adapter.namespace + '.connected', {val: true, ack: true});
+    });
 
-        function receive() {
-            cul.on('data', function (raw, obj) {
-                if (!obj || !obj.protocol || !obj.address) return;
-                var id = obj.protocol + '.' + obj.address;
-
-                for (var state in obj.data) {
-                    adapter.setState(id + '.' + state, {val:obj.data[state], ack: true});
-                }
-
-                if (!objects[id]) {
-                    var newObjects = [];
-                    var tmp = JSON.parse(JSON.stringify(obj));
-                    delete tmp.data;
-                    var newDevice = {_id: id, type: 'device', common: {name: obj.device + ' ' + obj.address}, native: tmp, children: []};
-                    for (var state in obj.data) {
-                        var common = {};
-
-                        if (metaRoles[obj.device + '_' + state]) {
-                            common = metaRoles[obj.device + '_' + state];
-                        } else if (metaRoles[state]) {
-                            common = metaRoles[state];
-                        }
-
-                        common.name = state + ' ' + obj.device + ' ' + id;
-
-                        newDevice.children.push(adapter.namespace + '.' + id + '.' + state);
-                        var newState = {
-                            _id: id + '.' + state,
-                            type: 'state',
-                            parent: adapter.namespace + '.' + id,
-                            common: common,
-                            native: {}
-                        };
-                        objects[id + '.' + state] = newState;
-                        newObjects.push(newState);
-                    }
-                    objects[id] = newDevice;
-                    newObjects.push(newDevice);
-
-
-                    function insertObjects() {
-                        if (newObjects.length < 1) {
-                            return;
-                        } else {
-                            var newObject = newObjects.pop();
-                            adapter.setObject(newObject._id, newObject, function (err, res) {
-                                adapter.log.info('object ' + adapter.namespace + '.' + newObject._id + ' created');
-                                insertObjects();
-                            });
-                        }
-                    }
-
-                    insertObjects();
-
-
-                }
-
-
-
+    function insertObjects(objs) {
+        if (objs.length < 1) {
+            return;
+        } else {
+            var newObject = objs.pop();
+            adapter.setObject(newObject._id, newObject, function (err, res) {
+                adapter.log.info('object ' + adapter.namespace + '.' + newObject._id + ' created');
+                setTimeout(insertObjects, 0, objs);
             });
         }
-
     }
 
-});
+    function receive() {
+        cul.on('data', function (raw, obj) {
+            if (!obj || !obj.protocol || !obj.address) return;
+            var id = obj.protocol + '.' + obj.address;
+
+            for (var state in obj.data) {
+                adapter.setState(id + '.' + state, {val: obj.data[state], ack: true});
+            }
+
+            if (!objects[id]) {
+                var newObjects = [];
+                var tmp = JSON.parse(JSON.stringify(obj));
+                delete tmp.data;
+                var newDevice = {
+                    _id:    id,
+                    type:   'device',
+                    common: {
+                        name: obj.device + ' ' + obj.address
+                    },
+                    native: tmp
+                };
+                for (var _state in obj.data) {
+                    var common = {};
+
+                    if (metaRoles[obj.device + '_' + _state]) {
+                        common = metaRoles[obj.device + '_' + _state];
+                    } else if (metaRoles[_state]) {
+                        common = metaRoles[_state];
+                    }
+
+                    common.name = _state + ' ' + obj.device + ' ' + id;
+
+                    var newState = {
+                        _id:    id + '.' + _state,
+                        type:   'state',
+                        common: common,
+                        native: {}
+                    };
+
+                    objects[id + '.' + _state] = newState;
+                    newObjects.push(newState);
+                }
+                objects[id] = newDevice;
+                newObjects.push(newDevice);
+
+                insertObjects();
+            }
+        });
+    }
+
+}
+
 
